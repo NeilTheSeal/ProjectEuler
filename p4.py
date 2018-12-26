f9dfbd6fb270910031eb93215cc8bb7883d53891
Neil Hendren <neil.hendren@gmail.com>
2018-12-25 23:38:41 -0700

    :D

--- p4.py
+++ p4.py
@@ -2,16 +2,27 @@ import math
 import scipy
 import numpy
 
+list1 = [1]
+list2 = [1, 1]
+
 
 class Pallin:
     def __init__(self, num):
         self.num = num
 
-    def ispal(self, num):
-        pal = int(str(num)[::-1])
-        if pal == num:
+    def ispal(self):
+        pal = int(str(self.num)[::-1])
+        if pal == self.num:
             return True
         else:
             return False
 
-    return
+
+for i in range(100, 999):
+    for k in range(100, 999):
+        if Pallin(i * k).ispal():
+            list1.append(i * k)
+            list2.append([i, k])
+
+print(max(list1))
+print(list2[list1.index(max(list1))])
